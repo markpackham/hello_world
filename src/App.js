8a21ee6205c7186570dce5ba8eec73622310d8d4
mark <markpackham1@gmail.com>
2020-07-06 11:02:31 +0100

    Add PiggyBank to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PiggyBank from './PiggyBank';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <PiggyBank />
   );
 }
 
---
+++ src/PiggyBank.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class PiggyBank extends Component{
+    render(){
+        return(
+            <h1>Hello World! I am a PiggyBank!</h1>
+        );
+    }
+}
+
+
+export default PiggyBank;
